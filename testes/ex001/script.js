dab20b4e5b2a01e043ab6333b9821f1d135e9433
Lucas Weidas <90000226+lucasweidas@users.noreply.github.com>
2022-01-07 21:04:02 -0300

    teste adicionado

--- testes/ex001/script.js
+++ testes/ex001/script.js
@@ -1,10 +1,35 @@
-function getSenha() {
-    var senha = '1234567';
-    return function verificarSenha(senhaP) {
-        if (senha === senhaP) return true;
-        return false;
-    };
+const body = document.body;
+const form = document.createElement('form');
+const password = document.createElement('input');
+const button = document.createElement('button');
+
+form.classList.add('form');
+form.onsubmit = e => e.preventDefault();
+
+password.classList.add('password');
+password.setAttribute('type', 'password');
+password.setAttribute('placeholder', 'Digite sua senha');
+
+button.classList.add('btn');
+button.innerText = 'Verificar';
+button.addEventListener('click', verificarSenha);
+
+form.appendChild(password);
+form.appendChild(button);
+body.appendChild(form);
+
+function verificarSenha() {
+    const senha = password.value.toString();
+    if (getSenha() === senha) {
+        password.classList.remove('invalido');
+        password.classList.add('valido');
+        return;
+    }
+    password.classList.remove('valido');
+    password.classList.toggle('invalido');
 }
 
-var verificar = getSenha();
-console.log(verificar('1234567'));
+function getSenha() {
+    const senha = 'teste123';
+    return senha;
+}
