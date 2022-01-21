a25ad7ccfdd1c46a4125c655fef9c759339f8edf
Lucas Weidas <90000226+lucasweidas@users.noreply.github.com>
2022-01-21 13:32:14 -0300

    update

--- scope/ex003/script.js
+++ scope/ex003/script.js
@@ -1,38 +1,10 @@
 // Exemplo
 
-// if (true) {
-//     let a = 10;
-//     const b = 20;
-//     if (true) {
-//         console.log(a);
-//         console.log(b);
-//     }
-// }
-
-/******************/
-// Exemplo
-
-// const a = 20;
-// {
-//     const a = 40;
-//     {
-//         const a = 60;
-//         console.log(a);
-//     }
-//     console.log(a);
-// }
-// console.log(a);
-
-/******************/
-// Exemplo
-
-// function outer() {
-
-//     function inner() {
-//         console.log(a);
-//     }
-//     return inner;
-// }
-// var a = 10;
-// var close = outer();
-// close();
+if (true) {
+    let a = 10;
+    const b = 20;
+    if (true) {
+        console.log(a);
+        console.log(b);
+    }
+}
