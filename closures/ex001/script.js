f0e3574b3d001b523b88fad7ba8c1aad2c249eb8
Lucas Weidas <90000226+lucasweidas@users.noreply.github.com>
2022-01-21 13:38:17 -0300

    novas pastas

---
+++ closures/ex001/script.js
@@ -0,0 +1,41 @@
+// Escopo Léxico e Closures
+
+const outer = () => {
+    const nome = "Lucas";
+    const inner = () => {
+        console.log(nome.toUpperCase());
+    }
+    inner();
+}
+outer();
+
+/***************/
+// Exemplo
+
+// const addTo = a => b => a + b;
+
+// const increment = addTo(1);
+// const increment10 = addTo(10);
+// const increment20 = addTo(20);
+
+// console.log(increment(5));
+// console.log(increment10(5));
+// console.log(increment20(5));
+
+/***************/
+// Exemplo
+
+// function closure() {
+//     for (var i = 1; i <= 5; i++) {
+
+//         (c => {
+//             setTimeout(() => {
+//                 console.log(c);
+//             }, c * 1000);
+//         })(i);
+//     }
+// }
+// closure();
+
+/***************/
+// Exemplo
