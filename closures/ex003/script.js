0b5522073659a8f554f27f7ef2a75c60cf73b6a8
Lucas Weidas <90000226+lucasweidas@users.noreply.github.com>
2022-01-22 14:14:42 -0300

    atualização nos estudos de closure

--- closures/ex003/script.js
+++ closures/ex003/script.js
@@ -2,7 +2,6 @@
 
 function closure() {
     for (var i = 1; i <= 5; i++) {
-
         (c => {
             setTimeout(() => {
                 console.log(c);
@@ -11,3 +10,17 @@ function closure() {
     }
 }
 closure();
+
+// O mesmo código também pode ser encrito desta forma
+/* 
+function closure() {
+    for (var i = 1; i <= 5; i++) {
+        function copia(c) {
+            setTimeout(function () {
+                console.log(c);
+            }, c * 1000);
+        }
+        copia(i);
+    }
+}
+closure(); */
