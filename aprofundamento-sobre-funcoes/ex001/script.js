4ed438e11907db847edcde6df0a40fa644d477db
Lucas Weidas <90000226+lucasweidas@users.noreply.github.com>
2022-01-23 15:00:50 -0300

    atualização nos textos

--- aprofundamento-sobre-funcoes/ex001/script.js
+++ aprofundamento-sobre-funcoes/ex001/script.js
@@ -1,5 +1,5 @@
 /*  O que é Function Statement (Function Declaration)?
-É a declaração de uma função com 'nome'. Em outras palavras, é a declaração normal de uma função, e será executada depois, quando for invocada 'chamada'. */
+É a declaração normal de uma função com 'nome'. Em outras palavras, é a declaração normal de uma função, e será executada depois, quando for invocada 'chamada'. */
 function a() {
     console.log('function statement');
 }
@@ -7,7 +7,7 @@ a();
 
 /*********************************************/
 /*  O que é Function Expression?
-É uma função 'anônima' atribuida a uma variável. Em outras palavras, é quando você declara uma variável e faz à atribuição de uma função sem nome (anônima) */
+É uma função 'anônima' atribuida a uma variável. Em outras palavras, é quando você declara uma variável e à atribui uma função sem nome (anônima). */
 var b = function () {
     console.log('function expression');
 };
@@ -15,20 +15,20 @@ b();
 
 /*********************************************/
 /*  Diferença entre Function Statement e Function Expression?
-A maior diferença entre as duas é o "Hoisting". Lembre-se de como o hoisting funciona. As funções recebem referência para seu bloco de código e as variáveis recebem o valor temporário "undefined". Se você "chamar" uma "function statement" antes da sua declaração no código, ela será executada normalmente. Se você "chamar" uma "function expression" antes da atribuição da função no código, você irá receber um erro "TypeError", pois você está tentando acessar uma função que ainda não foi atribuida à variável, pois à variável antes da atribuição da função, possui o valor "undefined" e undefined não é uma função. */
+A maior diferença entre as duas é o "Hoisting". Lembre-se de como o hoisting funciona. As funções recebem referência para seu bloco de código e as variáveis recebem o valor temporário "undefined". Se você "chamar" uma "function statement" antes da sua declaração no código, ela será executada normalmente. Se você "chamar" uma "function expression" antes da atribuição da função no código, você irá receber um erro, pois você está tentando acessar uma função que ainda não foi atribuida à variável, pois à variável antes da atribuição da função, possui o valor "undefined" e undefined não é uma função. */
 c();
 function c() {
     console.log('hoisted');
 }
 
-// d(); // TypeError: d is not a function
+// d(); // Erro
 var d = function () {
     console.log('não é hoisted');
 };
 
 /*********************************************/
 /*  O que é Anonymous Function?
-É uma função sem nome. Sempre que uma função sem nome é criada, isso é conhecida com uma função anônima (anonymous function). Função anônima não possui uma identidade, e caso você crie uma igual à abaixo, vai resultar em um "SyntaxError", pois de acordo com as epecificações da ECMA, function statement devem obrigatóriamente possuir um nome "identidade". */
+É uma função sem nome. Sempre que uma função sem nome é criada, isso é conhecido como uma função anônima (anonymous function). Função anônima não possui uma "identidade", e caso você tente criar uma como se fosse uma function statement sem nome, isso irá resultar em um "SyntaxError", pois de acordo com as epecificações da ECMA, function statement devem obrigatóriamente possuir um nome (identidade). */
 /* function () {
     console.log('função anônima que dá erro de Syntax');
 } */
@@ -36,23 +36,27 @@ var d = function () {
 /*********************************************/
 /*  Qual o uso para uma Anonymous Function?
 Funções anônimas são utilizadas em um lugar onde funções são usadas como valores. Isso diz respeito à quando você atribui uma função à um variável, no caso de uma function expression. */
-
-/*********************************************/
-/*  O que é Named Function Expression?
-Named function expression é básicamente o mesmo que uma "function expression". A diferença entre "named function expression" e "function expression", é que em uma named function expression, você atribui uma função com nome à uma variável. Diferente de uma function expression, onde você atribui uma função anônima. */
-var e = function nome() {
-    console.log('named function expression:', nome);
+var e = function () {
+    console.log('função anônima funcional');
 };
 e();
 
 /*********************************************/
+/*  O que é Named Function Expression?
+Named function expression é básicamente o mesmo que uma "function expression". A diferença entre as duas, é que em uma named function expression você atribui uma função com nome à uma variável, e com isso, você pode referenciar essa função dentro do seu escopo. Diferente de uma function expression, onde você atribui uma função anônima (sem nome) à uma variável. */
+var f = function comNome() {
+    console.log('named function expression:', comNome);
+};
+f();
+
+/*********************************************/
 /*  Diferença entre Parâmetros e Argumentos?
 "Parâmetros" são variável locais dentro do escopo da função, que recebem o valor passado como "argumento" na chamada da função. */
-function f(parametro) {
+function g(parametro) {
     console.log(parametro);
 }
-f(10);
+g(10);
 
 /*********************************************/
 /*  O que são First Class Functions?
-É à habilidade de usar funções como valores. Isso significa que uma função pode ser atribuida a uma variável, passada como argumento para outra função ou ser retorna de outra função. Essa habilidade é conhecida como first class functions em JavaScript. First class functions também são conhecidas com "first class citizens". */
+É à habilidade de usar funções como valores. Isso significa que uma função pode ser atribuida à uma variável, passada como argumento para outra função ou ser retornada de outra função. First class functions também são conhecidas como "first class citizens" em JavaScript. */
