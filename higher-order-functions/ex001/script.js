1b13bbfc745b901a0963b876e2b369afa11c3a86
Lucas Weidas <90000226+lucasweidas@users.noreply.github.com>
2022-02-01 15:43:19 -0300

    studies about Higher Order Functions

---
+++ higher-order-functions/ex001/script.js
@@ -0,0 +1,33 @@
+// Higher Order Functions:
+// This is when a function takes another function as an argument or returns another function.
+// The function passed as an argument to another function is known as a callback function.
+// This is only possible, because functions are First Class Citizens (First Class Function) in JavaScript.
+
+const radius = [3, 1, 2, 4];
+
+const area = function (radius) {
+    return Math.PI * radius * radius;
+};
+
+const circumference = function (radius) {
+    return 2 * Math.PI * radius;
+};
+
+const diameter = function (radius) {
+    return 2 * radius;
+};
+
+// This function is almost identical to the "map method"
+Array.prototype.calculate = function (logic) {
+    const output = [];
+    for (let i = 0; i < this.length; i++) {
+        output.push(logic(this[i]));
+    }
+    return output;
+};
+
+console.log(radius.map(area));
+
+console.log(radius.calculate(area));
+console.log(radius.calculate(circumference));
+console.log(radius.calculate(diameter));
