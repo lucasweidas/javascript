f61231cfe9da719e46b4bd4cf844136294dd7ff5
Lucas Weidas <90000226+lucasweidas@users.noreply.github.com>
2022-02-04 19:41:13 -0300

    test

--- testes/ex007/index.js
+++ testes/ex007/index.js
@@ -1,6 +1,38 @@
-function digital_root(n) {
-    n = String(n).split('').reduce((sum, num) => sum += parseInt(num), 0);
-    if (n > 9) return digital_root(n);
-    return n;
+function comp(array1, array2) {
+    if (array1 === null || array2 === null) return false;
+
+    return array2
+        .map(num => Math.sqrt(num))
+        .reduce((res, num) => {
+            if (array1.includes(num) && res) {
+                array1.splice(array1.indexOf(num), 1);
+                return true;
+            }
+            return false;
+        }, true);
+
+    // console.log(same);
+
+    // if (array2 === null || array2 === null) return false;
+
+    // return array2
+    //     .map(num => Math.sqrt(num))
+    //     .reduce((res, num) => {
+    //         if (!array1.includes(num)) res = false;
+    //         return res;
+    //     }, true);
 }
-console.log(digital_root(456));
+const a1 = [121, 144, 19, 161, 19, 144, 19, 11];
+const a2 = [
+    11 * 11,
+    121 * 121,
+    144 * 144,
+    19 * 19,
+    161 * 161,
+    19 * 19,
+    144 * 144,
+    19 * 19,
+];
+
+a1.inde
+console.log(comp(a1, a2));
