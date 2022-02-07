39ce8d49669f117cd4aa8d54cfbc500fcd0137d8
Lucas Weidas <90000226+lucasweidas@users.noreply.github.com>
2022-02-06 21:12:27 -0300

    up

--- testes/ex007/index.js
+++ testes/ex007/index.js
@@ -1,15 +1,18 @@
 function formatDuration(seconds) {
     seconds = 3661;
+    // Seconds
     if (seconds === 0) return 'now';
     if (seconds < 60) {
         return getSecTxt(seconds);
     }
+    // Minutes
     let sec = seconds % 60;
     let min = (seconds - sec) / 60;
     if (min < 60) {
         if (sec === 0) return getMinTxt(min);
         return `${getMinTxt(min)} and ${getSecTxt(sec)}`;
     }
+    // Hours
     let hrs = parseInt(min / 60);
     min %= 60;
     if (hrs <= 23) {
