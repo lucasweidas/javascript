65854e6ff17acdf6f012934f9cf207e28e0031ff
Lucas Weidas <90000226+lucasweidas@users.noreply.github.com>
2022-02-07 12:00:52 -0300

    new test

--- testes/ex007/index.js
+++ testes/ex007/index.js
@@ -1,67 +1,75 @@
 function formatDuration(seconds) {
-    seconds = 3661;
-    // Seconds
+    seconds = 31556952;
     if (seconds === 0) return 'now';
-    if (seconds < 60) {
-        return getSecTxt(seconds);
+
+    // Seconds
+    let sec = { time: 0, type: 'second' };
+    sec.time = seconds;
+    if (sec.time < 60) {
+        return getFormat(sec);
     }
+    let format = [];
+    sec.time %= 60;
+    format.push(sec);
+
     // Minutes
-    let sec = seconds % 60;
-    let min = (seconds - sec) / 60;
-    if (min < 60) {
-        if (sec === 0) return getMinTxt(min);
-        return `${getMinTxt(min)} and ${getSecTxt(sec)}`;
+    let min = { time: 0, type: 'minute' };
+    min.time = parseInt(seconds / 60);
+    format.push(min);
+    if (min.time < 60) {
+        return getReturn(format.reverse().map(unit => getFormat(unit)).filter(unit => unit !== null));
     }
+    min.time %= 60;
+
     // Hours
-    let hrs = parseInt(min / 60);
-    min %= 60;
-    if (hrs <= 23) {
-        if (min === 0) return `${getHrsTxt(hrs)}`;
-        if (sec === 0) return `${getHrsTxt(hrs)} and ${getMinTxt(min)}`;
-        return `${getHrsTxt(hrs)}, ${getMinTxt(min)} and ${getSecTxt(sec)}`;
+    let hrs = { time: 0, type: 'hour' };
+    hrs.time = parseInt(seconds / 3600);
+    format.push(hrs);
+    if (hrs.time < 24) {
+        return getReturn(format.reverse().map(unit => getFormat(unit)).filter(unit => unit !== null));
+    }
+    hrs.time %= 24;
+
+    // Days
+    let day = { time: 0, type: 'day' };
+    day.time = parseInt(seconds / 86400);
+    format.push(day);
+    if (day.time < 365) {
+        return getReturn(format.reverse().map(unit => getFormat(unit)).filter(unit => unit !== null));
+    }
+    day.time %= 365;
+
+    // Years
+    let year = { time: 0, type: 'year' };
+    year.time = parseInt(seconds / 31556952);
+    format.push(year);
+    return getReturn(format.reverse().map(unit => getFormat(unit)).filter(unit => unit !== null));
+}
+
+function getFormat(unit) {
+    switch (unit.time) {
+        case 0:
+            return null;
+        case 1:
+            return `${unit.time} ${unit.type}`;
+        default:
+            return `${unit.time} ${unit.type}s`;
     }
 }
 
-function getSecTxt(seconds) {
-    let txt = '';
-    switch (seconds) {
-        case 0:
-            break;
+function getReturn(arr) {
+    switch (arr.length) {
         case 1:
-            txt = `${seconds} second`;
-            break;
+            return arr[0];
+        case 2:
+            return `${arr[0]} and ${arr[1]}`;
+        case 3:
+            return `${arr[0]}, ${arr[1]} and ${arr[2]}`;
+        case 4:
+            return `${arr[0]}, ${arr[1]}, ${arr[2]} and ${arr[3]}`;
         default:
-            txt = `${seconds} seconds`;
+            return `${arr[0]}, ${arr[1]}, ${arr[2]}, ${arr[3]} and ${arr[4]}`;
     }
-    return txt;
-}
-
-function getMinTxt(minutes) {
-    let txt = '';
-    switch (minutes) {
-        case 0:
-            break;
-        case 1:
-            txt = `${minutes} minute`;
-            break;
-        default:
-            txt = `${minutes} minutes`;
-    }
-    return txt;
-}
-
-function getHrsTxt(hours) {
-    let txt = '';
-    switch (hours) {
-        case 0:
-            break;
-        case 1:
-            txt = `${hours} hour`;
-            break;
-        default:
-            txt = `${hours} hours`;
-    }
-    return txt;
 }
 
 console.log(formatDuration());
